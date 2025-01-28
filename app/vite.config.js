22904c64e8dc3d984b249d8df2ef51bb55d74f8e
alinasr783 <alinasreldin783@gmail.com>
2025-01-28 12:03:53 +0000

    Update vite config and paths for GitHub Pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // هنا يجب وضع اسم المستودع الخاص بك على GitHub
-})
+  base: '/rouver/', // اسم المستودع
+});
